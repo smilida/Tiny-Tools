c6936e9c5cfa67a74a6c0938c51c49ca3f758de3
smilida <40822014+smilida@users.noreply.github.com>
2023-01-04 19:48:19 +0800

    Update msa_visualization.R

--- r/use_ggmsa/msa_visualization.R
+++ r/use_ggmsa/msa_visualization.R
@@ -1,12 +1,3 @@
-## install proj4(required packages)
-conda install -c conda-forge proj4
-
-## install ggmsa(Use devtools or BiocManager)
-# based on devtools
-devtools::install_github("YuLab-SMU/ggmsa")
-# based on BiocManager
-BiocManager::install("ggmsa")
-
 ## Use ggmsa visualize MSA
 library("ggmsa")
 DNAMultipleAlignment <- Biostrings::readDNAMultipleAlignment("PATH/sample.fasta")
