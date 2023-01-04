7a1edc25fb161526e4738f6c80b34efa0addfe1b
smilida <40822014+smilida@users.noreply.github.com>
2023-01-04 19:45:59 +0800

    Update msa_visualization.R

--- r/use_ggmsa/msa_visualization.R
+++ r/use_ggmsa/msa_visualization.R
@@ -1,10 +1,13 @@
+## install proj4(required packages)
+conda install -c conda-forge proj4
+
 ## install ggmsa(Use devtools or BiocManager)
 # based on devtools
 devtools::install_github("YuLab-SMU/ggmsa")
 # based on BiocManager
 BiocManager::install("ggmsa")
 
-## Use ggmsa
-library(ggmsa)
-protein_sequences <- system.file("extdata", "sample.fasta", package = "ggmsa")
-ggmsa(protein_sequences, start = 221, end = 280, char_width = 0.5, seq_name = TRUE) + geom_seqlogo() + geom_msaBar()
+## Use ggmsa visualize MSA
+library("ggmsa")
+DNAMultipleAlignment <- Biostrings::readDNAMultipleAlignment("PATH/sample.fasta")
+ggmsa(DNAMultipleAlignment, 164, 213, color='Chemistry_NT')
