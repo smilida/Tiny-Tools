ad939607b3f0b8e483ca5dc3d2da254036d1f367
Yixiao Zhai <40822014+smilida@users.noreply.github.com>
2023-03-19 15:41:10 +0800

    Create hello.cpp

---
+++ conda/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "my_math.h"
+
+using namespace std;
+
+int main() {
+    int a = 2;
+    int b = 3;
+    cout << "Hello, world! " << add(a, b) << endl;
+    return 0;
+}
